b6b26b86ea569a8113ee47c1fa82e4a2f55e6eda
Benjamin Gayda-Knop <traxon.bg@gmail.com>
2023-10-31 16:39:50 +0100

    Fix TextStyleKonstanten

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'styles.dart'
 
 void main() {
   runApp(const AppAkademie());
---
+++ lib/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const headingStyle = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+
+const subHeadingStyle = TextStyle(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  color: Colors.black87,
+);
+
+const bodyTextStyle = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.normal,
+  color: Colors.black54,
+);
+
+const captionTextStyle = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.normal,
+  color: Colors.black38,
+);
